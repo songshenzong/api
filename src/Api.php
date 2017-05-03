98d53ed06782256e5b657b73d328e4eb27d9f338
connor <connor.niu@gmail.com>
2017-05-03 17:35:45 +0800

    fixed minor bugs

--- src/Api.php
+++ src/Api.php
@@ -895,7 +895,7 @@ class Api
         }
 
         if ($validator -> fails()) {
-            return $this -> setHttpStatusCode(env('SONGSHENZONG_API_VALIDATOR_STATUS_CODE', 422)) -> unprocessableEntity(env('SONGSHENZONG_API_VALIDATOR', 'Unprocessable Entity'), $validator -> errors());
+            return $this -> setHttpStatusCode(env('SONGSHENZONG_API_VALIDATOR_HTTP_STATUS_CODE', 422)) -> unprocessableEntity(env('SONGSHENZONG_API_VALIDATOR', 'Unprocessable Entity'), $validator -> errors());
         }
     }
 }
