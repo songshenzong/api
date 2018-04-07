1b7838bc0f22b512b28a5599681857150d0305f1
Songshenzong <i@songshenzong.com>
2018-04-07 11:08:40 +0800

    Fixed Bug for CORS

--- src/Middleware.php
+++ src/Middleware.php
@@ -82,7 +82,7 @@ class Middleware
             if (config('api.cors.credentials', true)) {
                 header('Access-Control-Allow-Credentials: true');
             }
-            header("Access-Control-Allow-Headers: Origin, X-Requested-With, No-Cache, Authorization, Content-Type, Accept");
+            header("Access-Control-Allow-Headers: Origin, X-Requested-With, No-Cache, Authorization, X-Auth-Token, Content-Type, Accept");
             header("Access-Control-Allow-Methods: GET, POST, HEAD, PUT, DELETE, PATCH, OPTIONS, TRACE");
             $age = config('api.cors.max_age', 86400);
             header("Access-Control-Max-Age: $age");
